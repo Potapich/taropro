74f78c382f34fdf219fc204b4b34da6438188184
Potapich <mihashadep@gmail.com>
2025-05-29 20:52:59 +0300

    get local from user Intl.DateTimeFormat().resolvedOptions().locale; / 29.05.2025

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const sv_db = require('../libs/db_lib');
 
-let advertising = false;
-
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.get('/getSpreads', async function (req, res) {
     if (typeof req.query.id !== 'undefined') {
-        let records = await sv_db.getSpreads(req.query.lang || "ru", req.query.id);
+        let locale = Intl.DateTimeFormat().resolvedOptions().locale;
+
+        let records = await sv_db.getSpreads(locale.substring(0, 2) || "ru", req.query.id);
         res.send(records);
     } else {
         res.status(400).json('error, no id');
@@ -21,7 +21,9 @@ router.get('/getSpreads', async function (req, res) {
 
 router.get('/getCards', async function (req, res) {
     if (typeof req.query.id !== 'undefined') {
-        let records = await sv_db.getCards(req.query.lang || "ru", req.query.id);
+        let locale = Intl.DateTimeFormat().resolvedOptions().locale;
+
+        let records = await sv_db.getCards(locale.substring(0, 2) || "ru", req.query.id);
         res.send(records);
     } else {
         res.status(400).json('error, no id');
@@ -29,17 +31,23 @@ router.get('/getCards', async function (req, res) {
 });
 
 router.get('/getSpreadsInfo', async function (req, res) {
-    let records = await sv_db.getSpreadsInfo(req.query.lang || "ru");
+    let locale = Intl.DateTimeFormat().resolvedOptions().locale;
+
+    let records = await sv_db.getSpreadsInfo(locale.substring(0, 2) || "ru");
     res.send(records);
 });
 
 router.get('/getCardsInfo', async function (req, res) {
-    let records = await sv_db.getCardsInfo(req.query.lang || "ru");
+    let locale = Intl.DateTimeFormat().resolvedOptions().locale;
+
+    let records = await sv_db.getCardsInfo(locale.substring(0, 2) || "ru");
     res.send(records);
 });
 
 router.get('/getContent', async function (req, res) {
-    let result = await sv_db.getContent(req.query.lang || "ru");
+    let locale = Intl.DateTimeFormat().resolvedOptions().locale;
+
+    let result = await sv_db.getContent(locale.substring(0, 2) || "ru");
     res.send(result);
 });
 
